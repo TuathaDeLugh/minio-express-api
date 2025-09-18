6e010fea928b4b4e9e7d89795410bee34402f9b5
Umang Sailor <ursailor@gmail.com>
2025-09-18 23:06:37 +0530

    .

--- src/config/minio.ts
+++ src/config/minio.ts
@@ -1,8 +1,8 @@
 import { Client } from "minio";
 
 const credenctials = {
-  endPoint: process.env.MINIO_ENDPOINT || "storage.umangsailor.com",
-  port: parseInt(process.env.MINIO_PORT || "443"),
+  endPoint: process.env.MINIO_ENDPOINT || "http://localhost",
+  port: parseInt(process.env.MINIO_PORT || "9000"),
   useSSL: process.env.MINIO_USE_SSL === "true",
   accessKey: process.env.MINIO_ACCESS_KEY || "admin",
   secretKey: process.env.MINIO_SECRET_KEY || "admin12345",
